5cfb00b10a37fa4ac4c7c3f461f88fbf92d242d8
Shravan Kuchkula <Shravan.Kuchkula@gmail.com>
2018-02-20 23:18:58 -0500

    User input to enter zipcode and visualize the weather

---
+++ loadWeatherData/sketch.js
@@ -0,0 +1,45 @@
+var weather;
+
+var api = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+//var zipcode = '07059';
+var country = ',us';
+var units = '&units=metric';
+var apiKey = '&appid=9cc3b51af34536d164280b4c8167b062';
+
+var input;
+
+function setup() {
+  createCanvas(400, 200);
+  
+  var button = select('#submit');
+  button.mousePressed(weatherAsk); 
+  
+  input = select('#city');
+
+}
+
+function weatherAsk(){
+  
+  var url = api + input.value() + country + units + apiKey;
+  loadJSON(url, gotData);
+  
+}
+
+function gotData(data) {
+  //print(data);
+  weather = data;
+}
+
+
+
+function draw() {
+  background(0);
+  
+  if (weather){
+    
+    ellipse(100, 100, weather.main.temp, weather.main.temp);
+    ellipse(300, 100, weather.main.humidity, weather.main.humidity);
+    
+  }
+  
+}
